4670a424c4e6a4aa41726077292a037ffefcde91
Anya Bhat <107057528+AnyaBhat@users.noreply.github.com>
2022-06-11 20:54:41 +0530

    Create Second Largest .cpp

---
+++ Second Largest .cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    int first = 0, second = -1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[first]) {
+            second = first;
+            first = i;
+        }
+        else if (arr[i] < arr[first]) {
+            if (second == -1 || arr[second] < arr[i])
+                second = i;
+        }
+    }
+    if (second>-1)
+       return arr[second];
+    return second;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
