414c7d6e06c632f85dd0ac81a9b6d6067caebab9
Ananya Bhat <anyabhat18@gmail.com>
2023-08-05 11:36:46 +0530

    Create Count_Digits.java

---
+++ Count_Digits.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.evenlyDivides(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    static int evenlyDivides(int N){
+        // code here
+        int r,temp=N,count=0;
+        while(temp>0){
+            r=temp%10;
+            if(r!=0){
+                if(N%r==0){
+                    count++;
+                }
+            }
+            temp/=10;
+        }
+        return count;
+    }
+}
